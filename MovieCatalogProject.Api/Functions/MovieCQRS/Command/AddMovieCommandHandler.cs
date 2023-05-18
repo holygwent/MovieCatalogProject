cf4a3f9d59c6b1a410160cd04e36184941f15534
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 18:13:37 +0200

    adding AddMovieCommandHandler.cs

---
+++ MovieCatalogProject.Api/Functions/MovieCQRS/Command/AddMovieCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using MovieCatalogProject.Domain.Common;
+using MovieCatalogProject.Domain.Entities;
+
+namespace MovieCatalogProject.Api.Functions.MovieCQRS.Command
+{
+    public record AddMovieCommand(string Name, DateTime ReleaseDate, string[] Genres) : IRequest;
+    public class AddMovieCommandHandler : IRequestHandler<AddMovieCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenericRepository<Movie> _movieRepository;
+
+        public AddMovieCommandHandler(IMapper mapper, IGenericRepository<Movie> movieRepository)
+        {
+            _mapper = mapper;
+            _movieRepository = movieRepository;
+        }
+
+        public async Task Handle(AddMovieCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var movie = _mapper.Map<Movie>(request);
+                await _movieRepository.AddAsync(movie);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+
+}
