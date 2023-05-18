6a5b090553a46f67853f7577a77c81d1097bad51
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 12:36:39 +0200

    update Program.cs
    adding infrastructure service to program.cs

--- MovieCatalogProject.Api/Program.cs
+++ MovieCatalogProject.Api/Program.cs
@@ -1,7 +1,9 @@
+using MovieCatalogProject.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddInfrastructureServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
