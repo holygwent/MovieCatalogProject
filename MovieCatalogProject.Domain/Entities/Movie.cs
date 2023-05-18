6083c596075c6fef617897dffc3256fd32aae4d7
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 12:21:01 +0200

    adding Movie.cs
    adding movie entity

---
+++ MovieCatalogProject.Domain/Entities/Movie.cs
@@ -0,0 +1,17 @@
+﻿using MovieCatalogProject.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogProject.Domain.Entities
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public Genres[] Genres { get; set; }
+    }
+}
