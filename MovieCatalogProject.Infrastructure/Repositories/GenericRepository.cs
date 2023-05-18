30adb0cda16dbe2c955e3dd80426c5cdb453fc86
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 14:05:35 +0200

    adding GenericRepository and GenericInterface

---
+++ MovieCatalogProject.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MovieCatalogProject.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogProject.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _entities;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _entities = _context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+            SaveChangesAsync().Wait();
+        }
+
+        public void Delete(T entity)
+        {
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Deleted;
+            SaveChangesAsync().Wait();
+
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(object id)
+        {
+            var entity = await _entities.FindAsync(id);
+            if (entity == null)
+                throw new NullReferenceException();
+            return entity;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+            entityEntry.State = EntityState.Modified;
+            SaveChangesAsync().Wait();
+
+        }
+    }
+}
