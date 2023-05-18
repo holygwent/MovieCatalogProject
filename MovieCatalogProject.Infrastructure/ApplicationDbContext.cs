ac21c119dda9cccbc79da7c4cc5c1cc868d7d9d9
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 12:35:56 +0200

    adding ApplicationDbContext.cs

---
+++ MovieCatalogProject.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieCatalogProject.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogProject.Infrastructure
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase(databaseName: "MovieCatalogProjectDatabase");
+        }
+    }
+}
