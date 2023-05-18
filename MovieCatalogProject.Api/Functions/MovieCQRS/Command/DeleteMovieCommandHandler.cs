dc1d24281596e29d3f0752888f41fdd01128327a
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 18:13:58 +0200

    adding DeleteMovieCommandHandler.cs

---
+++ MovieCatalogProject.Api/Functions/MovieCQRS/Command/DeleteMovieCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using MovieCatalogProject.Domain.Common;
+using MovieCatalogProject.Domain.Entities;
+
+namespace MovieCatalogProject.Api.Functions.MovieCQRS.Command
+{
+    public record DeleteMovieCommand(Guid id) : IRequest;
+    public class DeleteMovieCommandHandler : IRequestHandler<DeleteMovieCommand>
+    {
+        private readonly IGenericRepository<Movie> _movieRepository;
+
+        public DeleteMovieCommandHandler(IGenericRepository<Movie> movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public async Task Handle(DeleteMovieCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var movie = await _movieRepository.GetByIdAsync(request.id);
+                if (movie == null)
+                    throw new NullReferenceException($"there is no movie with id {request.id}");
+                _movieRepository.Delete(movie);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
