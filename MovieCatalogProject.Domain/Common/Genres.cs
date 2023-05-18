c98a6775f3c910c6000ade4849fd9c5745568b5b
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 12:21:16 +0200

    adding Genres.cs
    adding enum with genres

---
+++ MovieCatalogProject.Domain/Common/Genres.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieCatalogProject.Domain.Common
+{
+    public enum Genres
+    {
+        Horror,
+        Drama,
+        Comedy,
+        Action,
+        Crime,
+        Fantasy,
+        Western,
+    }
+}
