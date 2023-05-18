996f79f37482638603174b80118195329e79bec4
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-18 18:06:58 +0200

    Adding mapper and MediataR

---
+++ MovieCatalogProject.Api/MovieMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MovieCatalogProject.Api.Functions.MovieCQRS.Command;
+using MovieCatalogProject.Domain.Entities;
+
+namespace MovieCatalogProject.Api
+{
+    public class MovieMappingProfile:Profile
+    {
+        public MovieMappingProfile()
+        {
+            CreateMap<AddMovieCommand,Movie>()
+                .ForMember(m=>m.Id,c=>c.AllowNull());
+        }
+    }
+}
--- MovieCatalogProject.Api/Program.cs
+++ MovieCatalogProject.Api/Program.cs
@@ -1,9 +1,13 @@
+using MediatR;
 using MovieCatalogProject.Infrastructure;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructureServices();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
