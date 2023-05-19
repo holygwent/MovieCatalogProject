652130af17d6ee8a6181b78db43e631cf7768e43
Darius Cebrat <dcmieczyslaw@gmail.com>
2023-05-19 11:42:26 +0200

    adding GetMoviesByYearQueryHandler and GetMoviesByGenreQueryHandler

---
+++ MovieCatalogProject.Api/Functions/MovieCQRS/Query/GetMoviesByGenreQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using MovieCatalogProject.Domain.Common;
+using MovieCatalogProject.Domain.Entities;
+
+namespace MovieCatalogProject.Api.Functions.MovieCQRS.Query
+{
+    public record GetMoviesByGenreQuery(string genre):IRequest<GetMoviesByGenreQueryResponse>;
+    public class GetMoviesByGenreQueryHandler : IRequestHandler<GetMoviesByGenreQuery, GetMoviesByGenreQueryResponse>
+    {
+        private readonly IGenericRepository<Movie> _movieRepository;
+        public GetMoviesByGenreQueryHandler(IGenericRepository<Movie> movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public async Task<GetMoviesByGenreQueryResponse> Handle(GetMoviesByGenreQuery request, CancellationToken cancellationToken)
+        {
+            var movies = _movieRepository.GetAllAsync().Result.Where(x=>x.Genres.Contains(request.genre)).ToList();
+            return new GetMoviesByGenreQueryResponse(movies);
+        }
+    }
+    public record GetMoviesByGenreQueryResponse(List<Movie> movies);
+}
---
+++ MovieCatalogProject.Api/Functions/MovieCQRS/Query/GetMoviesByYearQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using MovieCatalogProject.Domain.Common;
+using MovieCatalogProject.Domain.Entities;
+
+namespace MovieCatalogProject.Api.Functions.MovieCQRS.Query
+{
+    public record GetMoviesByYearQuery(int year):IRequest<GetMoviesByYearQueryResponse>;
+    public class GetMoviesByYearQueryHandler : IRequestHandler<GetMoviesByYearQuery, GetMoviesByYearQueryResponse>
+    {
+        private readonly IGenericRepository<Movie> _movieRepository;
+        public GetMoviesByYearQueryHandler(IGenericRepository<Movie> movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public async Task<GetMoviesByYearQueryResponse> Handle(GetMoviesByYearQuery request, CancellationToken cancellationToken)
+        {
+            var movies = _movieRepository.GetAllAsync().Result.Where(x => x.ReleaseDate.Year == request.year).ToList();
+            return new GetMoviesByYearQueryResponse(movies);
+        }
+    }
+    public record GetMoviesByYearQueryResponse(List<Movie> Movies);
+}
